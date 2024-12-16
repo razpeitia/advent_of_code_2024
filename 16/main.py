c894d97c03e42243ab273e0dd7f025bd604b5743
Ricardo Azpeitia Pimentel <razpeitia@users.noreply.github.com>
2024-12-16 11:28:42 -0600

    Day 16 part 1

--- 16/main.py
+++ 16/main.py
@@ -1,18 +1,16 @@
 from collections import deque
 
-ROTATE = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+ROTATE_CLOCKWISE = {(0, 1): (1, 0), (1, 0): (0, -1), (0, -1): (-1, 0), (-1, 0): (0, 1)}
+ROTATE_COUNTERCLOCKWISE = {v: k for k, v in ROTATE_CLOCKWISE.items()}
 
 def read_file(filename):
     with open(filename, 'rt') as f:
         return [list(line.strip()) for line in f]
 
-def neighbors(i, j, di, dj, score):
-    x = ROTATE.index((di, dj))
-    clockwise = ROTATE[(x + 1) % len(ROTATE)]
-    counterclockwise = ROTATE[(x - 1) % len(ROTATE)]
-    return [((i+di, j+dj), (di, dj), score+1),
-            ((i, j), clockwise, score+1000),
-            ((i, j), counterclockwise, score+1000)]
+def neighbors(i, j, di, dj):
+    return [((i+di, j+dj), (di, dj), 1),
+            ((i, j), ROTATE_CLOCKWISE[di, dj], 1000),
+            ((i, j), ROTATE_COUNTERCLOCKWISE[di, dj], 1000)]
 
 def bfs(grid, s, direction, e):
     queue = deque([(s, direction, 0)])
@@ -24,15 +22,16 @@ def bfs(grid, s, direction, e):
         visited[key] = score if key not in visited else min(visited[key], score)
         if (i, j) == e:
             min_score = min(min_score, score)
-        for ((i, j), (di, dj), score) in neighbors(i, j, di, dj, score):
+        for ((i, j), (di, dj), weigth) in neighbors(i, j, di, dj):
             key = ((i, j), (di, dj))
             if grid[i][j] != '#':
+                new_score = score + weigth
                 if key not in visited:
-                    visited[key] = score
-                    queue.append(((i, j), (di, dj), score))
-                elif visited[key] > score:
-                    visited[key] = score
-                    queue.append(((i, j), (di, dj), score))
+                    visited[key] = new_score
+                    queue.append(((i, j), (di, dj), new_score))
+                elif visited[key] > new_score:
+                    visited[key] = new_score
+                    queue.append(((i, j), (di, dj), new_score))
     return min_score
 
 def part1(filename):
